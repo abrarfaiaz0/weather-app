c27eec5c4f9f5cf78e348d1350c282bdda3e385b
abrarfaiazatuno <abrarfaiaz0@gmail.com>
2023-02-11 23:08:26 +0600

    updated cards component

--- src/components/cards.js
+++ src/components/cards.js
@@ -2,6 +2,18 @@ import { useEffect, useState } from "react";
 
 function Cards(props) {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const arr_ = [
+    "Latitude",
+    "Longitude",
+    "Country",
+    "Temperature",
+    "Feels like",
+    "Humidity",
+    "Pressure",
+    "Summary",
+  ];
 
   useEffect(() => {
     getWeather(props.location);
@@ -12,14 +24,45 @@ function Cards(props) {
 
   async function getWeather(location) {
     const response = await fetch(
-      `http://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=36c242e887542108b600f50e82927a05&units=metric`
+      `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=36c242e887542108b600f50e82927a05&units=metric`
     );
     const json = await response.json();
+
+    const temp_arr = [];
+    const lat = json.coord.lat;
+    const lon = json.coord.lon;
+    const country = json.sys.country;
+    const temp = Math.round(json.main.temp);
+    const feels = Math.round(json.main.feels_like);
+    const humidity = json.main.humidity;
+    const pressure = json.main.pressure;
+    const weather = json.weather[0].main;
+
+    temp_arr.push(lat);
+    temp_arr.push(lon);
+    temp_arr.push(country);
+    temp_arr.push(temp);
+    temp_arr.push(feels);
+    temp_arr.push(humidity);
+    temp_arr.push(pressure);
+    temp_arr.push(weather);
+
+    setLoading(false);
+    setData(temp_arr);
+
+    console.log(data);
   }
 
+  if (loading) return <div> Loading... </div>;
   return (
     <div>
-      {data[0]}, {data[1]}, {data[2]}
+      {data.map((record, i) => {
+        return (
+          <div>
+            {arr_[i]} : {record}
+          </div>
+        );
+      })}
     </div>
   );
 }
