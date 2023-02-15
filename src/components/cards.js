8cfed2d5b8a92372f9af887310e01a1614c95c13
abrarfaiazatuno <abrarfaiaz0@gmail.com>
2023-02-15 19:32:43 +0600

    Revert "bug fixes"
    This reverts commit cacb929b006c935454c6750d75b4306ba0c5f623.

--- src/components/cards.js
+++ src/components/cards.js
@@ -22,7 +22,7 @@ function Cards(props) {
 
   async function getWeather(location) {
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=36c242e887542108b600f50e82927a05&units=metric`
+      `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=36c242e887542108b600f50e82927a05&units=metric`
     );
     const json = await response.json();
     setFound(!(json.cod === 404));
