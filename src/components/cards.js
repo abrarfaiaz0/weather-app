a599b6c0c0f01abe4e889f427884823d9d5aafe9
abrarfaiazatuno <abrarfaiaz0@gmail.com>
2023-02-15 16:44:33 +0600

    bug fix

--- src/components/cards.js
+++ src/components/cards.js
@@ -24,8 +24,8 @@ function Cards(props) {
       `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=36c242e887542108b600f50e82927a05&units=metric`
     );
     const json = await response.json();
-    setFound(json.cod === 200);
-    if (json.cod === 200) {
+    setFound(!(json.cod === 404));
+    if (!(json.cod === 404)) {
       const temp_arr = [];
       const lat = json.coord.lat;
       const lon = json.coord.lon;
